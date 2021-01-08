1de67c127704e3bf2b6909c440026e565b53c5dc
vanderreiffen <kevincachero@gmail.com>
2021-01-08 21:12:46 +0800

    added base code

--- main.py
+++ main.py
@@ -12,17 +12,41 @@ def load_to_array(csv_file):
 
 class BaseModel:
     def __init__(self, matrix_size):
+        # initialize graph
         self.matrix_size = matrix_size
         self.G = nx.grid_2d_graph(matrix_size, matrix_size)
         self.pos = dict((n, n) for n in self.G.nodes())
-        self.labels = dict(((i, j), i * 10 + j) for i, j in self.G.nodes())
+
+        # randomize nodes
+        for n in self.G.nodes():
+            self.G.nodes[n]['type'] = random.randint(0, 2)
+
+        self.labels = dict((n, 'X' if self.G.nodes[n]['type'] == 1 else 'O') for n in self.G.nodes())
+
+        self.type1_node_list = [n for (n, d) in self.G.nodes(data=True) if d['type'] == 1]
+        self.type2_node_list = [n for (n, d) in self.G.nodes(data=True) if d['type'] == 2]
+        self.empty_cells = [n for (n, d) in self.G.nodes(data=True) if d['type'] == 0]
 
     def display_graph(self):
+        nodes_y = nx.draw_networkx_nodes(self.G, self.pos, node_color='yellow', nodelist=self.type1_node_list)
+        nodes_b = nx.draw_networkx_nodes(self.G, self.pos, node_color='blue', nodelist=self.type2_node_list)
+        nodes_w = nx.draw_networkx_nodes(self.G, self.pos, node_color='white', nodelist=self.empty_cells)
+
+        # nx.draw(self.G, self.pos, with_labels=False)
+        nx.draw_networkx_edges(self.G, self.pos)
+        nx.draw_networkx_labels(self.G, self.pos, labels=self.labels)
+        plt.show()
+
+    def _calculate(self):
         pass
 
-    def calculate(self):
+
+class Schelling(BaseModel):
+    # def _
+    def _calculate(self):
         pass
 
 
 if __name__ == '__main__':
-    pass
+    schelling = Schelling(20)
+    schelling.display_graph()
