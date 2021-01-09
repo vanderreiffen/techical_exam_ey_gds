524c6bbf05001df5e6ba6679896cede10fcf8e76
vanderreiffen <kevincachero@gmail.com>
2021-01-09 15:10:28 +0800

    creating some tests

--- main.py
+++ main.py
@@ -4,103 +4,48 @@ import csv
 import matplotlib.pyplot as plt
 import random
 
-
+# item 1 on the exam
 def load_to_array(csv_file):
+    """ Loads CSV to array """
     data = list(csv.reader(open(csv_file)))
     return np.array(data)
 
 
-# class BaseModel:
-#     def __init__(self, matrix_size):
-#         # initialize graph
-#         self.matrix_size = matrix_size
-#         self.G = nx.grid_2d_graph(matrix_size, matrix_size)
-#         self.pos = dict((n, n) for n in self.G.nodes())
-#
-#         # randomize nodes
-#         for n in self.G.nodes():
-#             self.G.nodes[n]['type'] = random.randint(0, 2)
-#
-#         self.labels = dict((n, 'X' if self.G.nodes[n]['type'] == 1 else 'O') for n in self.G.nodes())
-#
-#         self.type1_node_list = [n for (n, d) in self.G.nodes(data=True) if d['type'] == 1]
-#         self.type2_node_list = [n for (n, d) in self.G.nodes(data=True) if d['type'] == 2]
-#         self.empty_cells = [n for (n, d) in self.G.nodes(data=True) if d['type'] == 0]
-#
-#         self.boundary_nodes_list = self._get_boundary_nodes(self.G)
-#         self.internal_nodes_list = list(set(self.G.nodes()) - set(self.boundary_nodes_list))
-#
-#     def display_graph(self):
-#         nodes_y = nx.draw_networkx_nodes(self.G, self.pos, node_color='yellow', nodelist=self.type1_node_list)
-#         nodes_b = nx.draw_networkx_nodes(self.G, self.pos, node_color='blue', nodelist=self.type2_node_list)
-#         nodes_w = nx.draw_networkx_nodes(self.G, self.pos, node_color='white', nodelist=self.empty_cells)
-#
-#         # nx.draw(self.G, self.pos, with_labels=False)
-#         nx.draw_networkx_edges(self.G, self.pos)
-#         nx.draw_networkx_labels(self.G, self.pos, labels=self.labels)
-#         plt.show()
-#
-#     def _get_boundary_nodes(self):
-#         nodes_list = []
-#         for (u, v) in self.G.nodes():
-#             if u == 0 or u == N - 1 or v == 0 or v == N - 1:
-#                 nodes_list.append((u, v))
-#         return nodes_list
-#
-#     def _get_neighbor_for_internal(self, x, y):
-#         return [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x - 1, y - 1),
-#                 (x + 1, y + 1)]
-#
-#     def get_neighbor_for_boundary(u, v):
-#         if u == 0 and v == 0:
-#             return [(0, 1), (1, 1), (1, 0)]
-#         elif u == N - 1 and v == N - 1:
-#             return [(N - 2, N - 2), (N - 1, N - 2), (N - 2, N - 1)]
-#         elif u == N - 1 and v == 0:
-#             return [(u - 1, v), (u, v + 1), (u - 1, v + 1)]
-#         elif u == 0 and v == N - 1:
-#             return [(u + 1, v), (u + 1, v - 1), (u, v - 1)]
-#         elif u == 0:
-#             return [(u, v - 1), (u, v + 1), (u + 1, v), (u + 1, v - 1), (u + 1, v + 1)]
-#         elif u == N - 1:
-#             return [(u - 1, v), (u, v - 1), (u, v + 1), (u - 1, v + 1), (u - 1, v - 1)]
-#         elif v == N - 1:
-#             return [(u, v - 1), (u - 1, v), (u + 1, v), (u - 1, v - 1), (u + 1, v - 1)]
-#         elif v == 0:
-#             return [(u - 1, v), (u + 1, v), (u, v + 1), (u - 1, v + 1), (u + 1, v + 1)]
-#
-#     def _calculate(self):
-#         pass
-#
-#
-# class Schelling(BaseModel):
-#     # def _
-#     def _calculate(self):
-#         pass
-
-
 def display_graph(G):
+    """ Load G (grid) customized it according to data types and displays the plot"""
+    # initialize graph
+    pos = dict((n, n) for n in G.nodes())
+    # create labels for nodes
+    labels = dict((n, G.nodes[n]['type']) for n in G.nodes())
+
+    # group node types according to list
+    type1_node_list = [n for (n, d) in G.nodes(data=True) if d['type'] == 1]
+    type2_node_list = [n for (n, d) in G.nodes(data=True) if d['type'] == 2]
+    empty_cells = [n for (n, d) in G.nodes(data=True) if d['type'] == 0]
+
+    # draw network and label nodes according to type
     nodes_y = nx.draw_networkx_nodes(G, pos, node_color='yellow', nodelist=type1_node_list)
     nodes_b = nx.draw_networkx_nodes(G, pos, node_color='blue', nodelist=type2_node_list)
     nodes_w = nx.draw_networkx_nodes(G, pos, node_color='white', nodelist=empty_cells)
 
-    # nx.draw(G, pos, with_labels=False)
     nx.draw_networkx_edges(G, pos)
     nx.draw_networkx_labels(G, pos, labels=labels)
     plt.show()
 
 
-def get_boundary_nodes(G):
+def get_boundary_nodes(G, max_row, max_col):
     nodes_list = []
-    for (u, v) in G.nodes():
-        if u == 0 or u == N-1 or v == 0 or v == N-1:
-            nodes_list.append((u,v))
+    for (x, y) in G.nodes():
+        if x == 0 or y == max_row-1 or y == 0 or x == max_col-1:
+            nodes_list.append((x, y))
     return nodes_list
+
 
 def get_neighbor_for_internal(x, y):
     return [(x-1, y), (x+1, y), (x, y+1), (x, y-1), (x-1, y+1) ,(x+1, y-1), (x-1, y-1), (x+1, y+1)]
 
-def get_neighbor_for_boundary(u, v):
+
+def get_neighbor_for_boundary(N, u, v):
     if u == 0 and v == 0:
         return [(0, 1), (1, 1), (1, 0)]
     elif u == N-1 and v == N - 1:
@@ -119,9 +64,21 @@ def get_neighbor_for_boundary(u, v):
         return [(u-1, v), (u+1, v), (u, v+1), (u-1, v+1), (u+1, v+1)]
 
 
-def get_unsatisfied_nodes_list(G, internal_nodes_list, boundary_nodes_list):
+def make_node_satisfied(unsatisfied_nodes_list, empty_cells):
+    if len(unsatisfied_nodes_list) != 0:
+        node_to_shift = random.choice(unsatisfied_nodes_list)
+        new_position = random.choice(empty_cells)
+
+        G.nodes[new_position]['type'] = G.nodes[node_to_shift]['type']
+        G.nodes[node_to_shift]['type'] = 0
+        labels[node_to_shift], labels[new_position] = labels[new_position], labels[node_to_shift]
+    else:
+        pass
+
+
+def get_unsatisfied_nodes_list(G, internal_nodes_list, boundary_nodes_list, threshold):
     unsatisfied_nodes_list = []
-    t = 3
+    t = threshold
     for (u, v) in G.nodes():
         type_of_this_node = G.nodes[(u, v)]['type']
         if type_of_this_node == 0:
@@ -142,26 +99,32 @@ def get_unsatisfied_nodes_list(G, internal_nodes_list, boundary_nodes_list):
 
     return unsatisfied_nodes_list
 
-def schelling_model(grid_source):
+
+# item 2
+def schelling_model(grid_source, threshold=3):
+    """ Shows schelling model """
     numrows = len(grid_source)
     numcols = len(grid_source[0])
 
+    # create grid
     G = nx.grid_2d_graph(numrows, numcols)
-    pos = dict((n, n) for n in G.nodes())
 
     for n in G.nodes():
         G.nodes[n]['type'] = random.randint(0, 2)
 
-    # create labels for nodes
-    labels = dict((n, G.nodes[n]['type']) for n in G.nodes())
+    # diagonal edges
+    # for ((x, y), d) in G.nodes(data=True):
+    #     if (u + 1 <= N - 1) and (v + 1 <= N - 1):
+    #         G.add_edge((u, v), (u + 1, v + 1))
+    # for ((u, v), d) in G.nodes(data=True):
+    #     if (u + 1 <= N - 1) and (v - 1 >= 0):
+    #         G.add_edge((u, v), (u + 1, v - 1))
 
-    # group node types according to list
-    type1_node_list = [n for (n, d) in G.nodes(data=True) if d['type'] == 1]
-    type2_node_list = [n for (n, d) in G.nodes(data=True) if d['type'] == 2]
-    empty_cells = [n for (n, d) in G.nodes(data=True) if d['type'] == 0]
+    # display initial graph
+    display_graph(G)
 
     # get boundary and internal noder
-    boundary_nodes_list = get_boundary_nodes(G)
+    boundary_nodes_list = get_boundary_nodes(G, numrows, numcols)
     internal_nodes_list = list(set(G.nodes()) - set(boundary_nodes_list))
 
     # make calculations according to threshold
@@ -171,16 +134,13 @@ def schelling_model(grid_source):
         unsatisfied_nodes_list = get_unsatisfied_nodes_list(G, internal_nodes_list, boundary_nodes_list)
         make_node_satisfied(unsatisfied_nodes_list, empty_cells)
 
-        type1_node_list = [n for (n, d) in G.nodes(data=True) if d['type'] == 1]
-        type2_node_list = [n for (n, d) in G.nodes(data=True) if d['type'] == 2]
-        empty_cells = [n for (n, d) in G.nodes(data=True) if d['type'] == 0]
-
+    # display final graph
     display_graph(G)
+
 
 if __name__ == '__main__':
     input_file = 'test.csv'
-    2d_grid = load_to_array(input_file)
+    grid = load_to_array(input_file)
 
     # show schelling model based on 2d grid
-    schelling_model
-    schelling.display_graph()
+    schelling_model(grid)
